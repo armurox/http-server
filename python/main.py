ef5bd03c5dbc3c19eac746ec33d4ec8a8bbf6ad9
Arman Jasuja <arman_jasuja@yahoo.com>
2025-04-05 00:45:16 +0400

    Add reflecting request body in response

--- python/main.py
+++ python/main.py
@@ -5,8 +5,21 @@ def main():
     connection, address = server.accept()
     print('Accepted connection from client with address', address)
     # Implement 200 or 404 at root address
-    request = connection.recv(1024).decode('utf-8').split()
-    connection.sendall(b'HTTP/1.1 200 OK\r\n\r\n' if request[1] == '/' else b'HTTP/1.1 404 Not Found\r\n\r\n')
+    request_path = connection.recv(1024).decode('utf-8').split()[1].split('/')
+    print(request_path)
+    try:
+        VALID_PATHS = {'': b'HTTP/1.1 200 OK\r\n\r\n', 
+                    'echo': 'HTTP/1.1 200 OK\r\n'
+                    'Content-Type: text/plain\r\n'
+                    f'Content-Length: {len(request_path[2])}\r\n'
+                    f'\r\n{request_path[2]}'.encode('utf-8')}
+    except IndexError:
+        VALID_PATHS = {'': b'HTTP/1.1 200 OK\r\n\r\n'}
+    if response := VALID_PATHS.get(request_path[1]):
+        pass
+    else:
+        response = b'HTTP/1.1 404 Not Found\r\n\r\n'
+    connection.sendall(response)
     
     
 if __name__ == '__main__':
