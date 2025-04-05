df369a2b96f6c74ed16aadfacaa75199200f82b7
Arman Jasuja <arman_jasuja@yahoo.com>
2025-04-05 19:14:09 +0400

    allow server to stay up for arbitrary amount of time

--- python/main.py
+++ python/main.py
@@ -4,10 +4,12 @@ from helpers import construct_response
 
 def main():
     server = socket.create_server(('localhost', 8080), reuse_port=True)
-    connection, address = server.accept()
-    print('Received connection from address %s', address)
-    request = parse_request(connection.recv(1024))
-    connection.sendall(construct_response(request))
+    while True:
+        connection, address = server.accept()
+        print('Received connection from address %s', address)
+        request = parse_request(connection.recv(1024))
+        connection.sendall(construct_response(request))
+        connection.close()
     
     
 if __name__ == '__main__':
